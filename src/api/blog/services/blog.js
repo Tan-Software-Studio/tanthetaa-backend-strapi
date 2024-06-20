ef20409ef177ad4edbcd7e8e56927d1eb018bf3f
CodebyAnandi <anandi.tanthetaa@gmail.com>
2024-06-20 17:32:47 +0530

    remove all changes

--- src/api/blog/services/blog.js
+++ src/api/blog/services/blog.js
@@ -7,12 +7,3 @@
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::blog.blog');
-
-
-const slugify = require('slugify');
-
-module.exports = {
-  generateSlug(title) {
-    return slugify(title, { lower: true });
-  },
-};
