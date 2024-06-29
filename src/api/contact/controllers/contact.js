7377a2dd5138775b6d4ddbb985217659ec9ce25e
CodebyAnandi <anandi.tanthetaa@gmail.com>
2024-06-29 11:09:50 +0530

    chnages in contact form for mediFiel

--- src/api/contact/controllers/contact.js
+++ src/api/contact/controllers/contact.js
@@ -1,13 +1,5 @@
-//  'use strict';
 
-//  /**
-//  * contact controller
-//   */
-
-// const { createCoreController } = require('@strapi/strapi').factories;
-
-//  module.exports = createCoreController('api::contact.contact');
-
+// //----------------------------------------------------------------------------------------------------------------------------------------------------------
 'use strict';
 
 /**
@@ -25,18 +17,19 @@ module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
     const response = await super.create(ctx);
 
     // Extract relevant data from the request
-    const { sendNda, email, name, projectDescription, budgetRange, budgetRangeInfo, contactInfo, socialInfo } = ctx.request.body.data;
-    console.log(sendNda, email, name, projectDescription, budgetRange, budgetRangeInfo, contactInfo, socialInfo);
-    console.log(ctx.request.body.data)
+    const { sendNda, email, name, projectDescription, budgetRange, budgetRangeInfo, contactInfo, socialInfo, mediaFiles } = ctx.request.body.data;
+    console.log(sendNda, email, name, projectDescription, budgetRange, budgetRangeInfo, contactInfo, socialInfo, mediaFiles);
+    console.log(ctx.request.body.data);
 
     // Check if the NDA checkbox is selected
     if (sendNda) {
       try {
         // Define the path to your NDA file
-        const filePath = path.resolve(__dirname, '../../../files/nda.pdf');
+        const ndaFilePath = path.resolve(__dirname, '../../../files/nda.pdf');
+        console.log(`NDA file path: ${ndaFilePath}`);
 
         // Read the NDA file
-        const fileContent = fs.readFileSync(filePath);
+        const ndaFileContent = fs.readFileSync(ndaFilePath);
 
         // Create nodemailer transporter (example with Ethereal)
         const transporter = nodemailer.createTransport({
@@ -48,7 +41,42 @@ module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
           },
         });
 
-        // Send the email with the NDA file attached to yourself
+        // Fetch media files from Strapi and construct attachments array
+        const attachments = [];
+
+        if (mediaFiles && mediaFiles.length > 0) {
+          for (const fileId of mediaFiles) {
+            const file = await strapi.entityService.findOne('plugin::upload.file', fileId);
+
+            if (file) {
+              // Construct the file path correctly
+              const mediaFilePath = path.join(__dirname, '../../../../public', file.url);
+              console.log(`Media file path: ${mediaFilePath}`);
+
+              // Check if the file exists
+              if (fs.existsSync(mediaFilePath)) {
+                // Read the file content
+                const mediaFileContent = fs.readFileSync(mediaFilePath);
+
+                // Add file to attachments
+                attachments.push({
+                  filename: file.name,
+                  content: mediaFileContent,
+                });
+              } else {
+                strapi.log.error(`Media file not found: ${mediaFilePath}`);
+              }
+            }
+          }
+        }
+
+        // Add NDA file to attachments
+        attachments.push({
+          filename: 'nda.pdf',
+          content: ndaFileContent,
+        });
+
+        // Send the email with the NDA file and media files attached to yourself
         await transporter.sendMail({
           from: `"Tantheta Software Studio" <${process.env.EMAIL_USER}>`,
           to: process.env.RECIPIENT_EMAIL,
@@ -83,15 +111,10 @@ module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
                 <td style="border: 1px solid #ddd; padding: 8px;">${JSON.stringify(socialInfo.socialUsername)}</td>
               </tr>
             </table>
-            <p>Attached is the NDA document requested by the user.</p>
+            <p>Attached is the NDA document requested by the user along with the provided media files.</p>
             <p>Best regards,<br>Tantheta Software Studio</p>
           `,
-          attachments: [
-            {
-              filename: 'nda.pdf',
-              content: fileContent
-            }
-          ]
+          attachments: attachments,
         });
 
         strapi.log.info(`NDA and user details sent to ${process.env.RECIPIENT_EMAIL}`);
@@ -104,118 +127,3 @@ module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
     return response;
   }
 }));
-
-
-
-// //----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-// 'use strict';
-
-// /**
-//  * contact controller
-//  */
-
-// const { createCoreController } = require('@strapi/strapi').factories;
-
-// module.exports = createCoreController('api::contact.contact');
-
-
-// 'use strict';
-
-// /**
-//  * contact controller
-//  */
-
-// const { createCoreController } = require('@strapi/strapi').factories;
-// const nodemailer = require('nodemailer');
-// const path = require('path');
-// const fs = require('fs');
-
-// module.exports = createCoreController('api::contact.contact', ({ strapi }) => ({
-//   async create(ctx) {
-//     // Call the default core action
-//     const response = await super.create(ctx);
-
-//     // Extract relevant data from the request
-//     const { sendNda, email, name, projectDescription, budgetRange } = ctx.request.body.data;
-//     console.log(sendNda, email, name, projectDescription, budgetRange);
-
-//     // Check if the NDA checkbox is selected
-//     if (sendNda) {
-//       try {
-//         // Define the path to your NDA file
-//         const filePath = path.resolve(__dirname, '../../../files/nda.pdf');
-
-//         // Read the NDA file
-//         const fileContent = fs.readFileSync(filePath);
-
-//         // const transporter = nodemailer.createTransport({
-//         //   host: 'smtp.gmail.com',
-//         //   port: 587,
-//         //   auth: {
-//         //     user: process.env.EMAIL_USER, // Your Gmail address
-//         //     pass: process.env.EMAIL_PASS, // Your Gmail password
-//         //   },
-//         // });
-
-//         const transporter = nodemailer.createTransport({
-//           host: 'smtp.ethereal.email',
-//           port: 587,
-//           auth: {
-//             user: process.env.EMAIL_USER, // Your Gmail address
-//             pass: process.env.EMAIL_PASS, // Your Gmail password
-//           },
-//         });
-
-//         // Send the email with the NDA file attached to yourself
-//         await transporter.sendMail({
-//           from: `"Tantheta Software Studio" <${process.env.EMAIL_USER}>`,
-//           to: process.env.RECIPIENT_EMAIL,
-//           subject: 'NDA Document and User Details',
-//           html: `
-//             <p>Hello,</p>
-//             <p>You have received a new NDA request along with user details. Please find the details below:</p>
-//             <h3>User Details:</h3>
-//             <table style="border-collapse: collapse; width: 100%;">
-//               <tr>
-//                 <td style="border: 1px solid #ddd; padding: 8px;"><strong>Name:</strong></td>
-//                 <td style="border: 1px solid #ddd; padding: 8px;">${name}</td>
-//               </tr>
-//               <tr>
-//                 <td style="border: 1px solid #ddd; padding: 8px;"><strong>Email:</strong></td>
-//                 <td style="border: 1px solid #ddd; padding: 8px;">${email}</td>
-//               </tr>
-//               <tr>
-//                 <td style="border: 1px solid #ddd; padding: 8px;"><strong>Project Description:</strong></td>
-//                 <td style="border: 1px solid #ddd; padding: 8px;">${projectDescription}</td>
-//               </tr>
-//               <tr>
-//                 <td style="border: 1px solid #ddd; padding: 8px;"><strong>Budget Range:</strong></td>
-//                 <td style="border: 1px solid #ddd; padding: 8px;">${budgetRange}</td>
-//               </tr>
-//             </table>
-//             <p>Attached is the NDA document requested by the user.</p>
-//             <p>Best regards,<br>Tantheta Software Studio</p>
-//           `,
-//           attachments: [
-//             {
-//               filename: 'nda.pdf',
-//               content: fileContent
-//             }
-//           ]
-//         });
-
-//         strapi.log.info(`NDA and user details sent to ${process.env.RECIPIENT_EMAIL}`);
-//       } catch (error) {
-//         strapi.log.error('Failed to send NDA:', error);
-//         ctx.throw(500, 'Failed to send NDA');
-//       }
-//     }
-
-//     return response;
-//   }
-// }));
-
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------------
